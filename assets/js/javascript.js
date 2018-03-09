be6147ddf50d4afaf06f7b27dc4fdbb872c77e05
sumathi <sumathi@rocketmail.com>
2018-03-08 19:43:45 -0500

    css and javascript for portfolio included

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,19 +2,14 @@ $(document).ready(function() {
 
         $(".imgPortfolio").on( "mouseenter", function(event){
             
-        //$(this).children(".classInfo").animate({height:'toggle'});
-
-        $(this).children(".classInfo").show(3000);
-            
+                // $(this).children(".classInfo").show(3000);
+        $(this).children(".classInfo").slideDown(1500);
         });
 
         $(".imgPortfolio").mouseleave(function hoverFunction(event){
             
-            // $(this).children(".classInfo").animate({
-            //     height:'toggle'
-            // });
-            $(this).children(".classInfo").hide(3000);
-            //$(this, ".classInfo").hide();
+               //$(this).children(".classInfo").hide(3000);
+            $(this).children(".classInfo").slideUp(300);
         });
 
 });
